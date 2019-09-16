96534c1187f09ac1ed61bc954c7de5bbea7d668a
Margot Henry <margothenry@live.com>
2019-09-16 17:11:51 -0500

    inital commit

---
+++ chapter 1.R
@@ -0,0 +1,28 @@
+#Exercise 1.1
+  #poisson
+  #binomial
+
+#Excercise 1.2
+  dbinom( 2, 10, 0.3) 
+  sum(dbinom(0:2, 10, 0.3))
+  pbinom( 2, 10, 0.3)
+  
+#Exercise 1.3
+  poisson_max_fucntion = function(n, m, lambda) {
+    prob = 1 - ppois( m-1, lambda )^n
+    return(prob)
+  }
+  # check that the function works with an example
+  poisson_max_fucntion(100, 7, 2)
+  
+#Exercise 1.4
+  poisson_max_fucntion = function(n = 100, m = 1, lambda = 5 ) {
+    prob = 1 - ppois( m-1, lambda )^n
+    return(prob)
+  } 
+  poisson_max_fucntion()
+#Exercise 1.5
+  poisson_max_fucntion(100, 9, 0.5)
+
+#Exercise 1.6
+  
